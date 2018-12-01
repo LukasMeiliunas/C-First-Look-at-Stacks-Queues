767ef6ae4fda1a100d9c4dc98850f42791994c6e
LukasMeiliunas <42815350+LukasMeiliunas@users.noreply.github.com>
2018-12-01 21:46:27 +0000

    Update StackDoubles.h

--- Lab7/StackDoubles.h
+++ Lab7/StackDoubles.h
@@ -5,12 +5,20 @@ using namespace std;
 
 class stackDoubles {
 public:
-	double a[MAX];
+	double a[MAX]; // max size 
+	
 	stackDoubles();
+	
 	bool push(int x);
+	
 	double pop();
-	bool isEmpty(); // same as top
-	void display();	void showTop();
+	
+	bool isEmpty(); //checks if stack is empty, if it is empty returns false
+	
+	void display();
+	
+	void showTop();
+
 private:
 	int top;
 };
